3c2fb569e876959665ec1b762f2250e8ba771634
River <1007941801@qq.com>
2023-12-13 14:18:44 +0800

    调试

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,5 +3,6 @@ export const PHONE_NUMBER = "18327533832";
 export const DEFAULT_PAGE_URL = "/main"; // 默认首页
 
 export const IFRAME_ID = "aIframe";
+// export const THAT_PAGE_URL = "http://localhost:3002";
 export const THAT_PAGE_URL = "https://hepengwei.cn";
 export const RECEIVE_KEY = 'aPageInfo';
